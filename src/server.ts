cc47cac8117eb298929bef5456e422c10e642cce
oliveiramiro2 <ramiromares95@gmail.com>
2023-01-18 05:34:12 -0300

    added routes to get a habits on day and register a new habit

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,12 @@
 import fastify from "fastify";
 import cors from "@fastify/cors"
-import { PrismaClient } from "@prisma/client"
+
+import { appRoutes } from "./routes";
 
 const app = fastify()
-const prisma = new PrismaClient()
 
 app.register(cors)
-
-app.get("/hello", () => {
-  const habits = prisma.habit.findMany()
-  return habits
-})
+app.register(appRoutes)
 
 app.listen({
   port: 3333
